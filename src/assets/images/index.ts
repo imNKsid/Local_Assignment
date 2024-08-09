277cc15885fa9192875ee977108f9b01f8b0aeec
Nitesh Kumar <imnksid@gmail.com>
2024-08-09 07:35:56 +0530

    Exporting location & whatsapp images

--- src/assets/images/index.ts
+++ src/assets/images/index.ts
@@ -1,4 +1,6 @@
 export const IMAGES = {
   arrowDown: require("./arrowDown.png"),
   arrowUp: require("./arrowUp.png"),
+  location: require("./location.png"),
+  whatsapp: require("./whatsapp.png"),
 };
