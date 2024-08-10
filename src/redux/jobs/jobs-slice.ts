b3f478f90d318f658e76f56aa6bcabd8fd129913
Nitesh Kumar <imnksid@gmail.com>
2024-08-10 22:05:16 +0530

    Removed jobsList setting as blank on pending, added safety check

--- src/redux/jobs/jobs-slice.ts
+++ src/redux/jobs/jobs-slice.ts
@@ -19,12 +19,11 @@ const JobsSlice = createSlice({
     builder
       .addCase(JobsThunk.getJobsData.pending, (state, action) => {
         state.getJobsSuccess = false;
-        state.jobsList = [];
         state.getJobsFailure = false;
       })
       .addCase(JobsThunk.getJobsData.fulfilled, (state, action) => {
         state.getJobsSuccess = true;
-        state.jobsList = action.payload.loadMore
+        state.jobsList = action?.payload?.loadMore
           ? [...state.jobsList, ...action.payload.data]
           : action.payload.data;
         state.getJobsFailure = false;
